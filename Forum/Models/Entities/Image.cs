c5723fd3b4035c809254c4ae4670aa5cc9255813
Anthony Minchenko <code.inside.us@gmail.com>
2020-08-24 02:30:01 +0300

    Created basic related entities and controllers

---
+++ Forum/Models/Entities/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models.Entities
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Filename { get; set; }
+    }
+}
