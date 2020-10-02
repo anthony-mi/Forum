5f98b05201e36ff991169c856241449112e4623d
Anthony Minchenko <code.inside.us@gmail.com>
2020-10-02 03:15:02 +0300

    Migrations

---
+++ Forum/Data/Migrations/20201001235119_AddAccessibility.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Forum.Data.Migrations
+{
+    public partial class AddAccessibility : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Accessibility",
+                table: "Topics",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Accessibility",
+                table: "Sections",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Accessibility",
+                table: "Topics");
+
+            migrationBuilder.DropColumn(
+                name: "Accessibility",
+                table: "Sections");
+        }
+    }
+}
