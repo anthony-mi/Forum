a3b7b9ee8bad3fb9c7b5aedfa8d5820c16b1c8dc
Anthony Minchenko <code.inside.us@gmail.com>
2020-10-19 00:53:58 +0300

    Created AccessibilityChecker service

---
+++ Forum/Services/AccessibilityChecker.cs
@@ -0,0 +1,20 @@
+﻿using Forum.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Services
+{
+    public class AccessibilityChecker
+    {
+        public static bool HasAccess(IList<string> userRoles, Accessibility accessibility)
+        {
+            return  accessibility == Accessibility.FullAccess ||
+                    (accessibility == Accessibility.OnlyForUsers && userRoles.Contains("User")) ||
+                    (accessibility == Accessibility.ReadonlyForUsers && userRoles.Contains("User"))
+                    || userRoles.Contains("Moderator")
+                    || userRoles.Contains("Admin");
+        }
+    }
+}
